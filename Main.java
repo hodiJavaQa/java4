8d8d3084593cc79531b8f5a9189f74b9ca878302
hodiJavaQa <99143515+hodiJavaQa@users.noreply.github.com>
2022-02-21 21:39:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,137 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum2=13;
+        //int number1 =-9;
+        //int number2 ,n1,n2,n3,n4,n5,n6;
+        //n6=-199;
+        //int number3=0;
+        //System.out.println(n6);
+        //number1 = -2;
+        //n6=254*80/4;
+        //number2=5;
+        //number1=number3+1;
+        //boolean flag = true;
+        //char c='+';
+        //String name="hi my name is hodi and i love java  5 * 5 = 12 ";
+        //System.out.println(name);
+        //name="hello guys";
+        //System.out.println(name);
+
+        //int x=18;
+        //int y=23;
+        //int z = x*12-y;
+//
+        //System.out.println(x);
+//
+        //int x=13;
+        //int y = 4500;
+        //double  max=Math.sqrt(13);
+//
+        //System.out.println(max);
+        //
+        //String s1="fake it until you make it";
+        //s1=s1+" God is Good";
+        //System.out.println(s1);
+        //Scanner sc1 = new Scanner(System.in);
+//
+        //System.out.print("please enter your name: ");
+        //String firtsName =sc1.next();
+        //System.out.print("please enter your age: ");
+        //int age = sc1.nextInt();
+//
+        //System.out.println(firtsName.toUpperCase()+" "+age);
+//
+        //for(int i=1;i<100;i++){
+        //    System.out.println(i);
+        //}
+        //System.out.println(1);
+        //System.out.println(2);
+        //System.out.println(3);
+        //System.out.println(3);
+        //System.out.println(4);
+        //System.out.println(5);
+
+        Scanner sc1 = new Scanner(System.in);
+        //System.out.println("please enter a number");
+        //int day= sc1.nextInt();
+        //int y = day%7;
+        //switch (y){
+        //    case 0 -> System.out.println("saturday");
+        //    case 1 -> System.out.println("Sunday");
+        //    case 2 -> System.out.println("monday");
+        //    case 3 -> System.out.println("tuesday");
+        //    case 4 -> System.out.println("wednesday");
+        //    case 5 -> System.out.println("thursday");
+        //    case 6 -> System.out.println("friday");
+        //}
+//
+        ////2
+        //System.out.println("enter the first number");
+        //int numb1=sc1.nextInt();
+        //System.out.println("enter the second number");
+        //int numb2=sc1.nextInt();
+//
+        //System.out.println(Math.max(numb1,numb2));
+//
+        ////3
+        //System.out.println("enter the first number");
+        //int numb3=sc1.nextInt();
+        //System.out.println(Math.pow(numb3,3));
+//
+        ////4
+        //for(int i = 0; i < 14; i++)
+        //{
+        //    System.out.println("hiiii");
+        //}
+
+        //6
+        //for(int i = 0; i <= 100 ; i++){
+        //    if(i % 3 == 0)
+        //        System.out.println(i);
+        //}
+//
+        ////method 2
+        //for(int i = 3; i <= 100 ; i+=3){
+        //    System.out.println(i);
+        //}
+//
+        //int sum=0;
+        //System.out.println(sum);
+        //for (int i = 0; i < 8; i++){
+        //    System.out.println("please enter a number");
+        //    int temp= sc1.nextInt();
+        //    System.out.println("your input is :"+temp);
+        //    sum=sum+temp;
+        //    System.out.println("sum is : "+sum);
+        //}
+//
+        //5!=5*4*3*2*1;
+        //100 =100*99*98*97*96........3*2*1;
+
+        //System.out.println(sum);
+
+
+        double avg=0;
+        for(int i =0 ; i< 3 ; i++ ){
+            System.out.print("please enter a number");
+            double temp = sc1.nextDouble();
+            avg=avg+temp;
+        }
+        System.out.println(avg/3);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
